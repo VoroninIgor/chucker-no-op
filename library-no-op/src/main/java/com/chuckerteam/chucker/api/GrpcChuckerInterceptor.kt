026ec230bf8747fe1e5ecd6b7baaeab46d8213df
angai <angainor17@gmail.com>
2020-10-07 13:11:47 +0300

    no-op

---
+++ library-no-op/src/main/java/com/chuckerteam/chucker/api/GrpcChuckerInterceptor.kt
@@ -0,0 +1,20 @@
+package com.chuckerteam.chucker.api
+
+import android.content.Context
+import io.grpc.CallOptions
+import io.grpc.Channel
+import io.grpc.ClientCall
+import io.grpc.ClientInterceptor
+import io.grpc.ForwardingClientCall
+import io.grpc.MethodDescriptor
+
+public class GrpcChuckerInterceptor(
+    private val context: Context,
+    private val collector: ChuckerCollector = ChuckerCollector(context),
+) : ClientInterceptor {
+    override fun <ReqT : Any?, RespT : Any?> interceptCall(method: MethodDescriptor<ReqT, RespT>?, callOptions: CallOptions?, next: Channel): ClientCall<ReqT, RespT> {
+        return object :
+            ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
+        }
+    }
+}
